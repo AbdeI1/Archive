5a509a3f423319c513820b6112db36f55b7de98b
AbdeI1 <83883159+AbdeI1@users.noreply.github.com>
2021-11-30 23:34:04 -0600

    Add files via upload

---
+++ AdventOfCode/2021/Day 1/Day1Code.py
@@ -0,0 +1,24 @@
+def reader():
+	f = open("Day1input.txt", 'r').read()
+	f = f.split('\n')
+	f = f[: len(f) - 1]
+	return f
+
+def part1():
+	f = reader()
+	count = 0
+	for i in range(len(f) - 1):
+		if(int(f[i+1]) > int(f[i])):
+			count+=1
+	print(count)
+
+def part2():
+	f = reader()
+	count = 0
+	for i in range(len(f) - 3):
+		if(int(f[i+1]) + int(f[i+2]) + int(f[i+3]) > int(f[i]) + int(f[i+1]) + int(f[i+2])):
+			count+=1
+	print(count)
+
+part1()
+part2()
