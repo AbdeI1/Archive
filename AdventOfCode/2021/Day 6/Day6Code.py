a59f5537c0cfe8f42394d2e7a3859ac38efd13a5
AbdeI1 <83883159+AbdeI1@users.noreply.github.com>
2021-12-17 20:26:24 -0600

    Add files via upload

--- AdventOfCode/2021/Day 6/Day6Code.py
+++ AdventOfCode/2021/Day 6/Day6Code.py
@@ -1,3 +1,5 @@
+from functools import *
+
 def reader():
 	f = open("Day6input.txt", 'r').read()
 	f = f.split('\n')
@@ -19,29 +21,37 @@ def part1():
 				timers[i] -= 1
 	print(fish)
 
-memo = [[-1]*500 for i in range(10)]
-
+@cache
 def simulate(n, t):
-	if memo[n][t] != -1:
-		return memo[n][t]
 	if t <= 0:
-		memo[n][t] = 1
 		return 1
 	if n == 0:
-		l = simulate(6,t-1) + simulate(8, t-1)
-		memo[n][t] = l
-		return l
-	l = simulate(n-1, t-1)
-	memo[n][t] = l
-	return l
+		return simulate(6,t-1) + simulate(8, t-1)
+	return simulate(n-1, t-1)
+
+s = [0]*9
+
+@cache
+def simulate2(n, t):
+	if t <= 0:
+		return s[n]
+	if n == 6:
+		return simulate2(7, t-1) + simulate2(0, t-1)
+	return simulate2((n+1)%9, t-1)
 
 def part2():
 	f = reader()
 	timers = list(map(int,f[0].split(',')))
-	sum = 0
 	for t in timers:
-		sum += simulate(t, 256)
-	print(sum)
+		s[t] += 1
+	s1 = 0
+	for t in timers:
+		s1 += simulate(t, 256)
+	s2 = 0
+	for i in range(9):
+		s2 += simulate2(i, 256)
+	print(s1)
+	print(s2)
 
 part1()
 part2()
